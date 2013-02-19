d3345957b7901f090149963843594d1ae17d7df8
Ricky Parkinson <rickyp72@googlemail.com>
2013-02-19 09:28:26 +0000

    initial commit

---
+++ nodes.pp
@@ -0,0 +1,51 @@
+node base {
+	include sudo, ssh
+	user { "cutter":
+	ensure => present,
+	home     => '/home/cutter',
+	password => 'Zfvu9HNYaaeHU',
+	shell    => '/bin/bash',
+
+}
+}
+
+node 'puppet.localdomain' inherits base {
+}
+node 'rnb-pec-01.localdomain' inherits base {
+	user { "pec.user":
+		comment => "pec user",
+		home => "/home/pec.user",
+		ensure => present,
+		password => '$1$RiBlfzOy$SugdkWGMJwXX8WFqCFW0./',
+		#shell => "/bin/bash",
+		#uid => '501',
+		#gid => '20'
+	}
+}
+node 'centosvm01.localdomain' inherits base {
+	tomcat7_rhel::tomcat_application { "my-web-application":
+	application_root => "/opt",
+	tomcat_user => "webuser",
+	tomcat_port => "8080",
+	jvm_envs => "-server -Xmx1024m -Xms128m -XX:MaxPermSize=256m -Dmy.java.opt=i_love_java -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=some.ip.address",
+	tomcat_manager => true,
+	tomcat_admin_user => "superuser",
+	tomcat_admin_password => "secretpassword",
+	jmx_registry_port => 10054,
+	jmx_server_port => 10053
+} 
+tomcat7_rhel::tomcat_application { "my-second-application":
+application_root => "/opt",
+tomcat_user => "webuser",
+tomcat_port => "8090",
+jvm_envs => "-server -Xmx1024m -Xms128m -XX:MaxPermSize=256m -Dmy.java.opt=i_love_scala -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=some.ip.address"
+}
+}
+node 'solaris-pec-01.localdomain' {
+}
+node 'lion-pec-01.localdomain' {
+	user { "ricky":
+	ensure => present,
+	password         => '$1$RiBlfzOy$SugdkWGMJwXX8WFqCFW0./',
+}
+}
